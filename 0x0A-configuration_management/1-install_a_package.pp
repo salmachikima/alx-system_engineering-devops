c2a6b42076e0fe09d5d3f585e9fc07ccb5f709c4
salma <salmamarokti888@gmail.com>
2025-01-10 19:29:35 -0800

    vhabvheaf

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,17 @@
 #Install flask version 2.1.0
 
-exec {'pip3 install flask':
-require => Exec['python-installed'],
-command => '/usr/bin/pip3 install flask==2.1.0'
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
 }
 
-exec {'python-installed':
-command => '/usr/bin/which python3'
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
