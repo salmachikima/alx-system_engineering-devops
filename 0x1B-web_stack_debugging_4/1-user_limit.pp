b37a9fbdeef36453bc70ecba5e2c2c3a8ae54a8e
Sukuna <salmamarokti888@gmail.com>
2025-03-07 04:48:00 +0000

    11111

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Change the OS configuration
+# From 5 to 50000 fot both hard and soft
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
